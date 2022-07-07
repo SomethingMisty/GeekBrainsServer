7c7ad1464e3d561f77f27075422bbd5bf605a24a
SomethingMisty <85935806+SomethingMisty@users.noreply.github.com>
2022-07-07 19:04:31 +0300

    Новое приложение

---
+++ Client VK/Models/Session.swift
@@ -0,0 +1,11 @@
+
+
+import Foundation
+
+class Session {
+    private init() {}
+    static let instance = Session()
+    
+    var token = "" // хранение токена в VK
+    var userId = 0 // хранение идентификатора пользователя VK
+}
